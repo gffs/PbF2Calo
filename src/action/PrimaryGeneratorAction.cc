e3f879217533687647f5faca8acc5dcb99e94688
gffs <gaffus@gmail.com>
2016-09-04 21:14:45 -0700

    cleanup

--- src/action/PrimaryGeneratorAction.cc
+++ src/action/PrimaryGeneratorAction.cc
@@ -2,6 +2,7 @@
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4SystemOfUnits.hh"
+#include "GeometryUtility.h"
 #include "JsonUtility.h"
 #include "PrimaryGeneratorAction.h"
 #include "Randomize.hh"
@@ -16,8 +17,9 @@ PrimaryGeneratorAction::PrimaryGeneratorAction(const json11::Json& cfg):
 
     fParticleGun->SetParticleEnergy(cfg_["energy"].number_value() * MeV);
 
-    fParticleGun->SetParticleMomentumDirection(
-            json11::Init<G4ThreeVector>::with(cfg_["momentum_direction"]));
+    fParticleGun->SetParticleMomentumDirection(json11::Init<G4ThreeVector>::with(
+        cfg_["momentum_direction"])
+    );
 }
 
 PrimaryGeneratorAction::~PrimaryGeneratorAction()
@@ -27,8 +29,9 @@ PrimaryGeneratorAction::~PrimaryGeneratorAction()
 
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-    fParticleGun->SetParticlePosition(
-            json11::Init<G4ThreeVector>::with(cfg_["particle_position"]));
+    fParticleGun->SetParticlePosition(json11::Init<G4ThreeVector>::with(
+        cfg_["particle_position"], [&](double x) { return x * mm; })
+    );
 
     fParticleGun->GeneratePrimaryVertex(anEvent);
 }
